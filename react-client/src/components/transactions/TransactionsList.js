6049e8bd5efb967cdd8b47298fe909d9ce52ecf5
Chad Altemose <chad@chadzilla.com>
2016-12-29 09:11:08 -0500

    prettied up transaction list styling

--- react-client/src/components/transactions/TransactionsList.js
+++ react-client/src/components/transactions/TransactionsList.js
@@ -1,18 +1,28 @@
 import React, { PropTypes } from 'react'
+import moment from 'moment'
+
+const prettyDate = dateStamp => (
+    moment(dateStamp).format('YYYY/MM/DD')
+)
+
+const prettyAmount = amount => (
+    Number(amount).toFixed(2)
+)
 
 const TransactionsList = ({ transactions }) => (
     <div>
         <h2>Transactions List</h2>
-        <ul>
+        <ul className="transaction-list">
             {transactions.allIds.map(id => {
                 const transaction = transactions.byId[id]
+                const classes = transaction.amount > 0 ? 'transaction deposit' : 'transaction'
                 return (
-                    <li key={transaction._id}>
-                        {transaction.date} ::
-                        {transaction.amount} ::
-                        {transaction.payee} ::
-                        {transaction.category} ::
-                        {transaction.account}
+                    <li key={transaction._id} className={classes}>
+                        <span className="transaction-date">{prettyDate(transaction.date)}</span>
+                        <span className="transaction-amount">{prettyAmount(transaction.amount)}</span>
+                        <span className="transaction-payee">{transaction.payee}</span>
+                        <span className="transaction-category">{transaction.category}</span>
+                        <span className="transaction-account">{transaction.account}</span>
                     </li>
                 )
             })}
